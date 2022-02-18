a415606326101f6ef528dbab19fcbc4ccc552e62
vscwartz <99963392+vscwartz@users.noreply.github.com>
2022-02-18 10:08:44 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
         <footer className="mt-5 text-center footer">
-            © 2022 Plutus Properties
+            © 2022, Plutus Properties
             </footer>
         </>
     )
