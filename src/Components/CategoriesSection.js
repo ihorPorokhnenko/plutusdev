26ee075e534876e5799ef0345526ead09fe41b7e
vscwartz <99963392+vscwartz@users.noreply.github.com>
2022-02-18 10:15:14 -0500

    Update CategoriesSection.js

--- src/Components/CategoriesSection.js
+++ src/Components/CategoriesSection.js
@@ -9,15 +9,15 @@ export default function CategoriesSection() {
   return (
     <div>
       <Container>
-      <h2 className="mt-4">Live Anywhere</h2>
-      <p className="heading-p">EXPLORE OUR SELECTION OF THE BEST PLACES</p>
+      <h2 className="mt-4">Modernizing Real Estate Transactions</h2>
+      <p className="heading-p">PURCHASE A PROPERTY IN ABOUT A WEEK</p>
     
         <Row className="mt-5">
         <Col sm={12} md={4} lg={4}>
           <Link to="/personal-rooms"><Card className="category-cards text-dark mt-3">
             <Card.Img variant="top" src={personalRoomsPic} className="category-img"/>
             <Card.Body>
-              <Card.Title>Personal Rooms</Card.Title>
+              <Card.Title>Condos</Card.Title>
               <Card.Text>
                 Find Rooms that you won't have to share with anyone
               </Card.Text>
@@ -28,7 +28,7 @@ export default function CategoriesSection() {
           <Link to="/family-apartments"><Card className="category-cards text-dark mt-3">
             <Card.Img variant="top" src={familyApartments} className="category-img"/>
             <Card.Body>
-              <Card.Title>Family Apartments</Card.Title>
+              <Card.Title>Single Family Homes</Card.Title>
               <Card.Text>
               Find rooms to share and live affordably with the perfect roommates.
               </Card.Text>
@@ -39,7 +39,7 @@ export default function CategoriesSection() {
           <Link to="/vacation-villas"><Card className="category-cards text-dark mt-3">
             <Card.Img variant="top" src={villas} className="category-img"/>
             <Card.Body>
-              <Card.Title>Villas For Vacation</Card.Title>
+              <Card.Title>Luxurious Residences</Card.Title>
               <Card.Text>
               Rent an entire house, where the owner has traveled or doesn't live there anymore.
               </Card.Text>
