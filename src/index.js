eb5942e221f5dc228f6ba2e1b7da276741cb93dd
Joshua Villamarzo <jvillamarzo@gmail.com>
2022-04-15 17:23:14 -0700

    Update font to Comfortaa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import './fonts/Comfortaa/Comfortaa-Regular.ttf';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
