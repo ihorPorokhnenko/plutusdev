78096ccff0b8c126b1ed571aeb1dc44311d89298
vscwartz <99963392+vscwartz@users.noreply.github.com>
2023-01-17 18:39:47 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = React.forwardRef((props, ref) => {
   return (
     <>
       <footer id="footer" ref={ref} className="mt-5 text-center footer">
-        © 2023, Plutus Properties | <a href="mailto:info@plutusproperties.org">info@plutusproperties.org</a> | <a href="tel:+12025272324">+1 (202) 527-2324</a>
+        © 2022, Plutus Properties | <a href="mailto:info@plutusproperties.org">info@plutusproperties.org</a> | <a href="tel:+12025272324">+1 (202) 527-2324</a>
         {props.children}
       </footer>
     </>
