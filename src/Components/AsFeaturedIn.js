9faa409a8cbb27650ee984d7d1dd8de6bcd68f6d
Joshua Villamarzo <jvillamarzo@gmail.com>
2023-05-12 14:42:36 -0700

    Add AsFeaturedIn component

---
+++ src/Components/AsFeaturedIn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card, Container, Col, Row } from "react-bootstrap";
+import {Link} from "react-router-dom";
+// import personalRoomsPic from '../pictures/personal-rooms.jpg'
+// import familyApartments from '../pictures/family-apartments.jpg'
+// import villas from '../pictures/villas.jpg'
+import condos from '../pictures/Condos.jpeg'
+import homes from '../pictures/single family homes.jpeg'
+import luxury from '../pictures/luxury residences.jpeg'
+
+export default function AsFeaturedIn() {
+  return (
+    <div>
+      <Container>
+      <h2 className="mt-4">As Featured In</h2>
+        <Row className="mt-5">
+        
+        </Row>
+      </Container>
+    </div>
+  );
+}
