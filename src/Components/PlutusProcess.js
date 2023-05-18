ff40ef3c42f324229ac0c9089adbe056c388a28d
Joshua Villamarzo <jvillamarzo@gmail.com>
2023-05-17 21:23:10 -0700

    Update plutus process image

--- src/Components/PlutusProcess.js
+++ src/Components/PlutusProcess.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Image, Col, Row } from "react-bootstrap";
-import processImg from '../pictures/plutus-process.svg'
+// import processImg from '../pictures/plutus-process.svg'
+import processImg2 from '../pictures/plutus-process2.svg'
 
 const PlutusProcess = React.forwardRef((props, ref) => {
   return (
@@ -8,7 +9,7 @@ const PlutusProcess = React.forwardRef((props, ref) => {
       <Container>
         <h2 className="mt-4 text-center">The Plutus Process</h2>
         <Row className="mt-5 justify-content-center">
-          <Image src={processImg} fluid></Image>
+          <Image src={processImg2} fluid></Image>
         </Row>
       </Container>
     </div>
